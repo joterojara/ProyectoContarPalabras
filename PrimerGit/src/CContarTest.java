0dbbc1041543467b5a8a27272ad93c2c2498e5c6
joterojara <joterojara@educa.madrid.org>
2020-05-24 19:09:15 +0200

    segundo commit del javadoc

--- PrimerGit/src/CContarTest.java
+++ PrimerGit/src/CContarTest.java
@@ -2,12 +2,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CContarTest {
+
+/**
+ * 
+ * @author Jesus Otero
+ *
+ */
+public class CContarTest {
 
 	@Test
 	void test() {
 		CContar contarTest = null;
-		
+		// Comprobamos las funciones para observar si funcionan correctamente
 		assertEquals(4,contarTest.contarNumeroPalabras("hola hola hola hola"));
 		assertEquals(5,contarTest.contarCaracteresTotal("plato"));
 
