3a726a6aa3518de6e5738178cf9214f49c789d08
joterojara <joterojara@educa.madrid.org>
2020-05-24 18:54:48 +0200

    Commit tras primer test del metodo contar palabras

---
+++ PrimerGit/src/CContarTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CContarTest {
+
+	@Test
+	void test() {
+		CContar contarTest = null;
+		
+		assertEquals(4,contarTest.contarNumeroPalabras("hola hola hola hola"));
+	}
+
+}
