d6c7afeba245dc78196d1825759ad0cc27c60dae
joterojara <joterojara@educa.madrid.org>
2020-05-24 19:11:31 +0200

    tercer commit del javadoc

--- PrimerGit/src/CContar.java
+++ PrimerGit/src/CContar.java
@@ -1,7 +1,15 @@
 
+/**
+ * 
+ * @author Jesus Otero
+ *
+ */
 public class CContar {
+	/**
+	 * array de todos los caracteres existentes
+	 */
 	static char[] letras = new char[65536];
-	
+	// cuenta todos los caracteres que hay en la cadena 
 	static int contarCaracteresTotal(String cadena) {
 		int contador=0;
 		for(int i =0;i<cadena.length();i++) {
@@ -11,7 +19,7 @@ public class CContar {
 	}
 		return contador;	
 	}
-	
+	// cuenta el numero de palabras que tiene la cadena proporcionada
 	static int contarNumeroPalabras(String cadena) {
 		int contador=1;
 		for(int i =0;i<cadena.length();i++) {
@@ -21,7 +29,7 @@ public class CContar {
 		return contador;
 	}
 	
-	
+	//cuenta la repeticion de los caracteres y los envia en forma de array
 	static int[] contarTodosCaracteres(String cadena) {
 		int contador[];
 		contador = new int[65536];
